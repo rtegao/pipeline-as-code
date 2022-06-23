0bee0330b898909f1ac1a702cb5f970c00ba3ab7
Tegao <rictegao@gmail.com>
2022-06-23 16:59:40 -0300

    feature: aws vpc with terraform

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,10 +1,36 @@
 variable "cidr_block" {
-    type = "string"
+    type        = string
     description = "VPC CIDR block - split and route IP addresses"
+    default     = "10.0.0.0/16"
 }
 
-variable vpc_name{
-    type = "string"
+variable "vpc_name"{
+    type        = string
     description = "VPC name"
-    default = "management"
+    default     = "management"
+}
+
+variable "profile"{
+    type        = string
+    description = "aws profile account"
+    default     = "pocs"
+}
+
+variable "shared_credentials_files" {
+    type        = list
+    description = "aws profile credentials"
+    default     = ["~/.aws/credentials"]          
+}
+
+variable author {
+    type        = string
+    description = "Provisioner Author"
+    default     = "Tegao"
+}
+
+
+variable region {
+    type = string
+    description = "aws workspace region"
+    default = "us-east-2"
 }
--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -1,6 +1,6 @@
-resource "vpc_aws" "management" {
+resource "aws_vpc" "management"  {
     cidr_block = var.cidr_block
-    enable_dns_hostname = true
+    enable_dns_hostnames = true
 
     tags = {
         Name = var.vpc_name
